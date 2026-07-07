c11736fadd839088bcff1ae3bd9e7d8ff5b02581
isacc03 <eess2990@gmail.com>
2026-07-07 15:36:52 +0900

    Update CNN visualization script

--- scripts/deep-learning/08-3_convolutional_neural_network_visualization.py
+++ scripts/deep-learning/08-3_convolutional_neural_network_visualization.py
@@ -2,23 +2,41 @@
 # Source: https://colab.research.google.com/drive/1gdcunVMIEHBhjHrBGVWomjK9Ae27MShu?usp=sharing
 # Generated from the sanitized notebook.
 
-# %% Cell 1
-import keras
+# # 합성곱 신경망의 시각화
 
-model = keras.models.load_model('best-cnn-model.keras')
+# %% Cell 1
+# 실행마다 동일한 결과를 얻기 위해 케라스에 랜덤 시드를 사용하고 텐서플로 연산을 결정적으로 만듭니다.
+import keras
+import tensorflow as tf
+
+keras.utils.set_random_seed(42)
+tf.random.set_seed(42)
+tf.config.experimental.enable_op_determinism()
+
+# ## 가중치 시각화
 
 # %% Cell 2
-print(model.layers)
+# 코랩에서 실행하는 경우에는 다음 명령을 실행하여 best-cnn-model.keras 파일을 다운로드받아 사용하세요.
+from urllib.request import urlretrieve
+urlretrieve('https://github.com/rickiepark/hg-mldl2/raw/main/best-cnn-model.keras', 'best-cnn-model.keras')
 
 # %% Cell 3
+import keras
+model = keras.models.load_model('best-cnn-model.keras')
+
+# %% Cell 4
+print(model.layers)
+
+# %% Cell 5
 conv = model.layers[0]
 print(conv.weights[0].shape, conv.weights[1].shape)
 
-# %% Cell 4
+# %% Cell 6
 conv_weights = conv.weights[0].numpy()
+
 print(conv_weights.mean(), conv_weights.std())
 
-# %% Cell 5
+# %% Cell 7
 import matplotlib.pyplot as plt
 
 plt.hist(conv_weights.reshape(-1, 1))
@@ -26,83 +44,87 @@ plt.xlabel('weight')
 plt.ylabel('count')
 plt.show()
 
-# %% Cell 6
-fig, axs = plt.subplots(2, 16, figsize=(15, 2))
+# %% Cell 8
+fig, axs = plt.subplots(2, 16, figsize=(15,2))
 for i in range(2):
     for j in range(16):
-        axs[i, j].imshow(conv_weights[:, :, 0, i * 16 + j], vmin=-0.5, vmax=0.5)
+        axs[i, j].imshow(conv_weights[:,:,0,i*16 + j], vmin=-0.5, vmax=0.5)
         axs[i, j].axis('off')
 plt.show()
 
-# %% Cell 7
+# %% Cell 9
 no_training_model = keras.Sequential()
-no_training_model.add(keras.layers.Input(shape=(28, 28, 1)))
-no_training_model.add(keras.layers.Conv2D(32, kernel_size=3, activation='relu', padding='same'))
+no_training_model.add(keras.layers.Input(shape=(28,28,1)))
+no_training_model.add(keras.layers.Conv2D(32, kernel_size=3, activation='relu',
+                                          padding='same'))
 
-# %% Cell 8
+# %% Cell 10
 no_training_conv = no_training_model.layers[0]
 print(no_training_conv.weights[0].shape)
 
-# %% Cell 9
+# %% Cell 11
 no_training_weights = no_training_conv.weights[0].numpy()
 print(no_training_weights.mean(), no_training_weights.std())
 
-# %% Cell 10
+# %% Cell 12
 plt.hist(no_training_weights.reshape(-1, 1))
 plt.xlabel('weight')
 plt.ylabel('count')
 plt.show()
 
-# %% Cell 11
-fig, axs = plt.subplots(2, 16, figsize=(15, 2))
+# %% Cell 13
+fig, axs = plt.subplots(2, 16, figsize=(15,2))
 for i in range(2):
     for j in range(16):
-        axs[i, j].imshow(no_training_weights[:, :, 0, i * 16 + j], vmin=-0.5, vmax=0.5)
+        axs[i, j].imshow(no_training_weights[:,:,0,i*16 + j], vmin=-0.5, vmax=0.5)
         axs[i, j].axis('off')
 plt.show()
 
-# %% Cell 12
+# ## 함수형 API
+
+# %% Cell 14
 inputs = keras.Input(shape=(784,))
 dense1 = keras.layers.Dense(100, activation='relu')
 dense2 = keras.layers.Dense(10, activation='softmax')
 
-# %% Cell 13
 hidden = dense1(inputs)
-
-# %% Cell 14
 outputs = dense2(hidden)
 
-# %% Cell 15
 func_model = keras.Model(inputs, outputs)
 
-# %% Cell 16
+# %% Cell 15
 print(model.inputs)
 
-# %% Cell 17
+# %% Cell 16
 conv_acti = keras.Model(model.inputs[0], model.layers[0].output)
 
+# ## 특성 맵 시각화
+
+# %% Cell 17
+(train_input, train_target), (test_input, test_target) =\
+    keras.datasets.fashion_mnist.load_data()
+
 # %% Cell 18
-(train_input, train_target), (test_input, test_target) = keras.datasets.fashion_mnist.load_data()
 plt.imshow(train_input[0], cmap='gray_r')
 plt.show()
 
 # %% Cell 19
-ankle_boot = train_input[0:1].reshape(-1, 28, 28, 1) / 255.0
+ankle_boot = train_input[0:1].reshape(-1, 28, 28, 1)/255.0
 feature_maps = conv_acti.predict(ankle_boot)
 
 # %% Cell 20
 print(feature_maps.shape)
 
 # %% Cell 21
-fig, axs = plt.subplots(4, 8, figsize=(15, 8))
+fig, axs = plt.subplots(4, 8, figsize=(15,8))
 for i in range(4):
     for j in range(8):
-        axs[i, j].imshow(feature_maps[0, :, :, i * 8 + j])
+        axs[i, j].imshow(feature_maps[0,:,:,i*8 + j])
         axs[i, j].axis('off')
 plt.show()
 
 # %% Cell 22
-conv2_acti = keras.Model(model.inputs[0], model.layers[2].output)
+conv2_acti = keras.Model(model.inputs, model.layers[2].output)
 
 # %% Cell 23
 feature_maps = conv2_acti.predict(ankle_boot)
@@ -111,19 +133,30 @@ feature_maps = conv2_acti.predict(ankle_boot)
 print(feature_maps.shape)
 
 # %% Cell 25
-fig, axs = plt.subplots(8, 8, figsize=(12, 12))
+fig, axs = plt.subplots(8, 8, figsize=(12,12))
 for i in range(8):
     for j in range(8):
-        axs[i, j].imshow(feature_maps[0, :, :, 8 * i + j])
+        axs[i, j].imshow(feature_maps[0,:,:,i*8 + j])
         axs[i, j].axis('off')
 plt.show()
 
-# %% Cell 26
-from urllib.request import urlretrieve
+# # 합성곱 신경망의 시각화 (파이토치)
 
-urlretrieve('https://bit.ly/3DQeEH8', 'best_cnn_model.pt')
+# %% Cell 26
+# 실행마다 동일한 결과를 얻기 위해 파이토치에 랜덤 시드를 지정하고 GPU 연산을 결정적으로 만듭니다.
+import torch
+
+torch.manual_seed(42)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(42)
+    torch.backends.cudnn.deterministic = True
 
 # %% Cell 27
+# 코랩에서 실행하는 경우에는 다음 명령을 실행하여 best_cnn_model.pt 파일을 다운로드받아 사용하세요.
+from urllib.request import urlretrieve
+urlretrieve('https://github.com/rickiepark/hg-mldl2/raw/refs/heads/main/best_cnn_model.pt', 'best_cnn_model.pt')
+
+# %% Cell 28
 import torch.nn as nn
 
 model = nn.Sequential()
@@ -139,32 +172,30 @@ model.add_module('relu3', nn.ReLU())
 model.add_module('dropout', nn.Dropout(0.3))
 model.add_module('dense2', nn.Linear(100, 10))
 
-# %% Cell 28
-import torch
-
+# %% Cell 29
 model.load_state_dict(torch.load('best_cnn_model.pt', weights_only=True))
 
-# %% Cell 29
+# %% Cell 30
 layers = [layer for layer in model.children()]
 
-# %% Cell 30
+# %% Cell 31
 print(layers[0])
 
-# %% Cell 31
+# %% Cell 32
 print(model[0])
 
-# %% Cell 32
+# %% Cell 33
 for name, layer in model.named_children():
     print(f"{name:10s}", layer)
 
-# %% Cell 33
+# %% Cell 34
 print(model.conv1)
 
-# %% Cell 34
+# %% Cell 35
 conv_weights = model.conv1.weight.data
 print(conv_weights.mean(), conv_weights.std())
 
-# %% Cell 35
+# %% Cell 36
 import matplotlib.pyplot as plt
 
 plt.hist(conv_weights.reshape(-1, 1))
@@ -172,45 +203,47 @@ plt.xlabel('weight')
 plt.ylabel('count')
 plt.show()
 
-# %% Cell 36
+# %% Cell 37
 print(conv_weights.shape)
 
-# %% Cell 37
-fig, axs = plt.subplots(2, 16, figsize=(15, 2))
+# %% Cell 38
+fig, axs = plt.subplots(2, 16, figsize=(15,2))
 for i in range(2):
     for j in range(16):
-        axs[i, j].imshow(conv_weights[16 * i + j, 0, :, :], vmin=-0.5, vmax=0.5)
+        axs[i, j].imshow(conv_weights[i*16 + j,0,:,:], vmin=-0.5, vmax=0.5)
         axs[i, j].axis('off')
 plt.show()
 
-# %% Cell 38
+# %% Cell 39
 from torchvision.datasets import FashionMNIST
 
 fm_train = FashionMNIST(root='.', train=True, download=True)
 train_input = fm_train.data
+
+# %% Cell 40
 plt.imshow(train_input[0], cmap='gray_r')
 plt.show()
 
-# %% Cell 39
-ankle_boot = train_input[0:1].reshape(-1, 1, 28, 28) / 255.0
+# %% Cell 41
+ankle_boot = train_input[0:1].reshape(1, 1, 28, 28) / 255.0
 
 model.eval()
 with torch.no_grad():
     feature_maps = model.conv1(ankle_boot)
     feature_maps = model.relu1(feature_maps)
 
-# %% Cell 40
+# %% Cell 42
 print(feature_maps.shape)
 
-# %% Cell 41
-fig, axs = plt.subplots(4, 8, figsize=(15, 8))
+# %% Cell 43
+fig, axs = plt.subplots(4, 8, figsize=(15,8))
 for i in range(4):
     for j in range(8):
-        axs[i, j].imshow(feature_maps[0, i * 4 + j, :, :])
+        axs[i, j].imshow(feature_maps[0,i*8 + j,:,:])
         axs[i, j].axis('off')
 plt.show()
 
-# %% Cell 42
+# %% Cell 44
 model.eval()
 with torch.no_grad():
     feature_maps = model.conv1(ankle_boot)
@@ -219,7 +252,7 @@ with torch.no_grad():
     feature_maps = model.conv2(feature_maps)
     feature_maps = model.relu2(feature_maps)
 
-# %% Cell 43
+# %% Cell 45
 model.eval()
 x = ankle_boot
 with torch.no_grad():
@@ -229,10 +262,10 @@ with torch.no_grad():
             break
 feature_maps = x
 
-# %% Cell 44
-fig, axs = plt.subplots(8, 8, figsize=(12, 12))
+# %% Cell 46
+fig, axs = plt.subplots(8, 8, figsize=(12,12))
 for i in range(8):
     for j in range(8):
-        axs[i, j].imshow(feature_maps[0, i * 8 + j, :, :])
+        axs[i, j].imshow(feature_maps[0,i*8 + j,:,:])
         axs[i, j].axis('off')
 plt.show()
